ba8009b86930038c18fe1be878313c5870c97437
Jeroen Verstraete <Jeroen.Verstraete@kuleuven.be>
2021-11-23 14:39:53 +0100

    start of SiouxFalls od

---
+++ DemandCSV.py
@@ -0,0 +1,10 @@
+import numpy as np
+from dyntapy.demand import od_graph_from_matrix
+from numpy import genfromtxt
+from dyntapy.networks.get_networks import get_toy_network
+g = get_toy_network('siouxfalls')
+
+my_data = genfromtxt('SiouxFalls_od.csv', delimiter=',', skip_header=1)
+od = my_data.copy()
+od[:, (0, 1)] -= 1
+g_od = od_graph_from_matrix(od, g.nodes.data('x_coord'), g.nodes.data('y_coord'))
